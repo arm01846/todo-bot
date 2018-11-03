6eaa0f873081994431b59086bd6c0b9ca524418b
Sukrit Amornpornwiwat <arm01846@gmail.com>
2018-11-03 20:28:26 +0700

    fix time format

--- src/main/kotlin/bot/todo/Command.kt
+++ src/main/kotlin/bot/todo/Command.kt
@@ -15,7 +15,7 @@ object Command {
             else -> LocalDate.parse(token[1].trim(), DateTimeFormatter.ofPattern("d/M/yy"))
         }
 
-        val time = LocalTime.parse(token[2].trim(), DateTimeFormatter.ofPattern("HH:mm"))
+        val time = LocalTime.parse(token[2].trim(), DateTimeFormatter.ofPattern("H:m"))
 
         return Todo(message, LocalDateTime.of(date, time))
     }
--- src/test/kotlin/bot/todo/CommandTest.kt
+++ src/test/kotlin/bot/todo/CommandTest.kt
@@ -15,6 +15,11 @@ class CommandTest {
         )
 
         assertEquals(
+                Todo("Buy milk", LocalDateTime.of(2018, 5, 3, 13, 0)),
+                Command.parse("Buy milk : 03/05/18 : 13:00")
+        )
+
+        assertEquals(
                 Todo("Finish writing shopping list", LocalDate.now().atTime(15, 30)),
                 Command.parse("Finish writing shopping list : today : 15:30")
         )
@@ -22,6 +27,11 @@ class CommandTest {
         assertEquals(
                 Todo("Watch movie", LocalDate.now().plusDays(1).atTime(18, 0)),
                 Command.parse("Watch movie : tomorrow : 18:00")
+        )
+
+        assertEquals(
+                Todo("Watch movie", LocalDateTime.of(2018, 12, 30, 8, 0)),
+                Command.parse("Watch movie : 30/12/18 : 8:00")
         )
     }
 }
