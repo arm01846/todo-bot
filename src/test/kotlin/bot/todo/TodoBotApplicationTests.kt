9f8ae0f168f42aa2e2250674336f0b41a213ed20
Sukrit Amornpornwiwat <arm01846@gmail.com>
2018-11-03 12:14:01 +0700

    initialize project

---
+++ src/test/kotlin/bot/todo/TodoBotApplicationTests.kt
@@ -0,0 +1,16 @@
+package bot.todo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TodoBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
