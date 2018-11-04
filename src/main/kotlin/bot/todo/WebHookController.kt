e0fed7d6fade78e17a8b715b8e2c4f385f71beb3
Sukrit Amornpornwiwat <arm01846@gmail.com>
2018-11-04 10:36:05 +0700

    validate line signature of request from webhook

--- src/main/kotlin/bot/todo/WebHookController.kt
+++ src/main/kotlin/bot/todo/WebHookController.kt
@@ -1,23 +1,44 @@
 package bot.todo
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.linecorp.bot.model.event.MessageEvent
 import com.linecorp.bot.model.event.message.MessageContent
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
+import javax.crypto.Mac
+import javax.crypto.spec.SecretKeySpec
+
+
 
 @RestController
 class WebHookController (
-        val todoService: TodoService
+        val todoService: TodoService,
+        val lineConfiguration: LineConfiguration
 ) {
+
+    val jsonMapper = ObjectMapper()
 
     @PostMapping(value = ["/line/webhook"])
     @ResponseStatus(value = HttpStatus.OK)
-    fun handleLineWebHook(@RequestBody body: LineWebHookRequest) {
+    fun handleLineWebHook(
+            @RequestHeader(value = "X-Line-Signature") signature: String,
+            @RequestBody body: LineWebHookRequest) {
+        if (!validateSignature(signature, jsonMapper.writeValueAsString(body))) {
+            return
+        }
+
         val event = body.events[0]
         todoService.process(event)
+    }
+
+    fun validateSignature(signature: String, body: String): Boolean {
+        val key = SecretKeySpec(lineConfiguration.lineToken.toByteArray(), "HmacSHA256")
+        val mac = Mac.getInstance("HmacSHA256")
+        mac.init(key)
+        val source = body.toByteArray()
+        val calculatedSignature = Base64.getEncoder().encode(mac.doFinal(source))
+        return signature.toByteArray().contentEquals(calculatedSignature)
     }
 }
 
